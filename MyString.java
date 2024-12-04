d47ab2e176f52d67bc4b2a9a7fce261df39e4ea3
adam <mada.carpm>
2024-12-04 15:14:08 +0200

    strings 2

--- MyString.java
+++ MyString.java
@@ -6,14 +6,22 @@ public class MyString {
         System.out.println("TLV : " + lowerCase("TLV"));
         System.out.println("lowercase : " + lowerCase("lowercase"));
 
-        System.out.println("Testing contains:");
-        System.out.println(contains("unhappy", "happy")); // true
-        System.out.println(contains("happy", "unhappy")); // false
-        System.out.println(contains("historical", "story")); // false
-        System.out.println(contains("psychology", "psycho")); // true
-        System.out.println(contains("personality", "son")); // true
-        System.out.println(contains("personality", "dad")); // false
-        System.out.println(contains("resignation", "sign")); // true
+        System.out.println("Testing contains:");/*
+                                                 * /
+                                                 * System.out.println(contains("unhappy", "happy")); // true
+                                                 * System.out.println(contains("happy", "unhappy")); // false
+                                                 * System.out.println(contains("historical", "story")); // false
+                                                 * System.out.println(contains("psychology", "psycho")); // true
+                                                 * System.out.println(contains("personality", "son")); // true
+                                                 * System.out.println(contains("personality", "dad")); // false
+                                                 * System.out.println(contains("resignation", "sign")); // true
+                                                 */
+        System.out.println(contains("baba yaga", "baba"));
+        System.out.println(contains("baba yaga", ""));
+        System.out.println(contains("baba yaga", "John Wick is the baba yaga"));
+        System.out.println(contains("baba yaga", "Yaga"));
+        System.out.println(contains("baba yaga", "babayaga"));
+
     }
 
     /** Returns the lowercase version of the given string. */
@@ -25,13 +33,15 @@ public class MyString {
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
+        if (str2.length() == 0){return false;}
+        str1 = lowerCase(str1);
+        str2 = lowerCase(str2);
         for (int i = 0; i < str1.length(); i++) {
             if (str1.charAt(i) == str2.charAt(0)) {
                 int j = 0;
                 while (j < str2.length()) {
-                    if (i + j +1> str1.length()) {
+                    if (i + j + 1 > str1.length()) {
                         break;
-                        // make sure the index doesnt distroy me
                     }
                     if (str1.charAt(i + j) == str2.charAt(j)) {
                         j++;
